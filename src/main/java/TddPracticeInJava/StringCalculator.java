6bb1b5089a6bac7661f5a37c83bc0c89f20380da
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-08 10:52:18 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -12,7 +12,7 @@ public class StringCalculator{
     return -1;
   }
 
-  protected static boolean isEmptyString(Stirng str){
+  protected static boolean isEmptyString(String str){
     if (str == null || str=="") {
         return true; // The string is empty
     }
