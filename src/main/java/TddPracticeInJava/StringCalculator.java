413d6e9cd8dde993a2839bc16cd78304981aa3b0
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-08 10:34:00 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -3,9 +3,11 @@ package TddPracticeInJava;
 
 public class StringCalculator{
 
-  public static int add(String value){
+  public static int add(String str){
     int result=-1;
-   
+   if (str == NULL || str[0] == '\0') {
+        return 1; // The string is empty
+    }
     return result;
   }
   
