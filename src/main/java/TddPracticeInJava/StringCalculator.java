f602b76487720445a0d9e14a3cc40664bfb25291
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-10 14:34:47 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -4,17 +4,46 @@ package TddPracticeInJava;
 public class StringCalculator{
 
   public static int add(String str){
+    int result=-1;
 
     if(isEmptyString(str)|| str=="0"){
         return 0;
     }
 
-    
+    result=splitAndAddTheNumbers(str);
    
-    return -1;
+    return result;
   }
 
   protected static int splitAndAddTheNumbers(str){
+        String delimiter = ",";
+        String numbersInput = numbersStr;
+
+        // Check for custom delimiter
+        if (numbersInput.startsWith("//")) {
+            int delimiterEnd = numbersInput.indexOf("\n");
+            delimiter = numbersInput.substring(2, delimiterEnd);
+            numbersInput = numbersInput.substring(delimiterEnd + 1);
+        }
+
+        // Replace new lines with delimiter
+        numbersInput = numbersInput.replace("\n", delimiter);
+
+        // Split string by delimiter
+        String[] numbersArray = numbersInput.split(Pattern.quote(delimiter));
+        List<Integer> negatives = new ArrayList<>();
+        int totalSum = 0;
+
+        // Sum numbers and handle negatives
+        for (String numberStr : numbersArray) {
+            int num = Integer.parseInt(numberStr);
+            if (num < 0) {
+                negatives.add(num);
+            } else if (num <= 1000) {
+                totalSum += num;
+            }
+        }
+    return totalSum;
   }
 
   protected static boolean isEmptyString(String str){
