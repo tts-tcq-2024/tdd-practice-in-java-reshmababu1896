895294337df2fe2bcfc24266c5b73325498fb5a1
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-10 15:53:39 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public class StringCalculator{
   public static int totalSum=0;
-  // public static List<Integer> negatives = new ArrayList<>();
 
   public static int add(String str){
       int result=-1;
@@ -21,31 +20,27 @@ public class StringCalculator{
   protected static int splitAndAdd(String str){
         String delimiter = ",";
         String numbersInput = str;
-
-        // Check for custom delimiter
         if (numbersInput.startsWith("//")) {
             int delimiterEnd = numbersInput.indexOf("\n");
             delimiter = numbersInput.substring(2, delimiterEnd);
             numbersInput = numbersInput.substring(delimiterEnd + 1);
         }
-
-        // Replace new lines with delimiter
         numbersInput = numbersInput.replace("\n", delimiter);
 
-        // Split string by delimiter
         String[] numbersArray = numbersInput.split(Pattern.quote(delimiter));
-      totalSum=addNumbers(numbersArray); 
+        totalSum=addNumbers(numbersArray); 
     return totalSum;
   }
 
 protected static int  addNumbers(String[] numbersArray){
-   // Sum numbers and handle negatives
+   
         for (String numberStr : numbersArray) {
             int num = Integer.parseInt(numberStr);
            totalSum=verifyNumber(num);
         }
   return totalSum;
   }
+  
 protected static int verifyNumber(int num){
     if (num >0  && num <= 1000) {
                 totalSum += num;
