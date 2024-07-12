239b2a1e77093c16374c5b53755aee7ea02aeec8
reshmababu1896 <reshmababu1896@gmail.com>
2024-07-12 16:18:29 +0530

    Update StringCalculator.java

--- src/main/java/TddPracticeInJava/StringCalculator.java
+++ src/main/java/TddPracticeInJava/StringCalculator.java
@@ -6,52 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator{
-  public static int totalSum=0;
+   public static int totalSum = 0;
 
-  public static int add(String str){
-      int result=-1;
-      if(isEmptyString(str) || str=="0"){
-          return 0;
-      }
-      result=splitAndAdd(str);
-      return result;
+    public static int add(String str) {
+        int result = -1;
+        if (isEmptyString(str) || str == "0") {
+            return 0;
+        }
+        result = splitAndAdd(str);
+        return result;
     }
 
-  protected static int splitAndAdd(String str){
-    // String  result =(number.replace(/^[^0-9]+/, '')).split(/,|;|\n/);
-     String[] numbersArray = str.split("[\n;*&//]");
-    return addNumbers(numbersArray);
-  }
-
-  
-
-protected static int  addNumbers(String[] numbersArray){
-      for (String num : numbersArray) {
-        try{
-          int number = Integer.parseInt(num);
-          totalSum=verifyNumber(number);
-        }
-        catch(NumberFormatException  e){
-          continue;
-        }
-      }
-  return totalSum;
-  }
-  
-protected static int verifyNumber(int num){
-    if (num >0  && num <= 1000) {
-                totalSum += num;
-       }
-  return totalSum;
-  }
-  
-
-  protected static boolean isEmptyString(String str){
-    if (str == null || str=="" ) {
-        return true; // The string is empty
+    protected static int splitAndAdd(String str) {
+        String result = str.replaceAll("[\\-\\+\\.\\^\\\n\\;\\//:,]", "");
+        char[] numbersArray = result.toCharArray();
+        System.out.println("After removing special characters" + numbersArray);
+        return addNumbers(numbersArray);
     }
-    return false;
-  }
+
+    protected static int addNumbers(char[] numbersArray) {
+        for (char num : numbersArray) {
+            try {
+                totalSum=0;
+                int number = Integer.parseInt(String.valueOf(num));
+                totalSum = verifyNumber(number);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+        }
+        return totalSum;
+    }
+
+    protected static int verifyNumber(int num) {
+        if (num > 0 && num <= 1000) {
+            totalSum += num;
+        }
+        return totalSum;
+    }
+
+
+    protected static boolean isEmptyString(String str) {
+        if (str == null || str == "") {
+            return true; // The string is empty
+        }
+        return false;
+    }
   
 }
 
